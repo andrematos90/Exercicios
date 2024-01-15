fdecc343a0d8670eb0682dbe23948f49074427a6
André Matos <andrermatos90@gmail.com>
2024-01-15 16:53:18 -0300

    dados no template

--- exercicio/src/app/app.module.ts
+++ exercicio/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataManipulationComponentComponent } from './components/data-manipulation-component/data-manipulation-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataManipulationComponentComponent,
+
   ],
   imports: [
     BrowserModule,
