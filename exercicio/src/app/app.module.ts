4ed7fda632714a710bfd9c9805b8b35ca30bf03a
André Matos <andrermatos90@gmail.com>
2024-01-16 16:25:10 -0300

    compartilhamento de dados entre pai e filho

--- exercicio/src/app/app.module.ts
+++ exercicio/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaiComponentComponent } from './components/pai-component/pai-component.component';
+import { FilhoComponentComponent } from './components/filho-component/filho-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaiComponentComponent,
+    FilhoComponentComponent
   ],
   imports: [
     BrowserModule,
