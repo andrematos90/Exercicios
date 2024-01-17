c376458767428e0413b7a139afc44800d23bcea8
André Matos <andrermatos90@gmail.com>
2024-01-17 15:49:06 -0300

    exercicio sobre diretivas no angular

--- exercicio/src/app/app.component.ts
+++ exercicio/src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exercicio';
+
+  animais = [
+    {nome: "Simba", raca: "Rough-Collie", showRaca:false},
+    {nome:"Milu", raca: "Vira-Lata", showRaca:false},
+    {nome:"Luna", raca: "Pastor", showRaca:false},
+    {nome:"Pretinha", raca: "Vira-Lata", showRaca:false},
+    {nome:"Sky", raca: "Podle", showRaca:false},
+    {nome:"Pitú", raca: "Podle", showRaca:false},
+    {nome:"Xuxa", raca: "Podle", showRaca:false}
+  ]
+
+
+
+
+  showDetail(animal:any):void{
+    animal.showRaca = !animal.showRaca
+  }
 }
